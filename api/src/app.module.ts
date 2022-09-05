7adf18dcc9166010556cab4b8c7d42b5727e8505
Nathan Hui <huinathanc@gmail.com>
2022-09-05 10:29:31 +0800

    publish branch

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoginModule } from './login/login.module';
+import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
+import { NotifsModule } from './notifs/notifs.module';
 
 @Module({
-  imports: [],
+  imports: [LoginModule, UsersModule, PostsModule, NotifsModule],
   controllers: [AppController],
   providers: [AppService],
 })
